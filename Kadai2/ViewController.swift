149c72d7bc2ba566c0ac1032e740233749ada0cd
Ryoh <Ryoh.popy@gmail.com>
2021-07-04 21:51:47 +0900

    [Add] 機能要件の実装

--- Kadai2/ViewController.swift
+++ Kadai2/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var num1TextField: UITextField!
+    @IBOutlet private weak var num2TextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var operatorSegmentedControl: UISegmentedControl!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    enum OperatorEnum: Int {
+        case plus
+        case minus
+        case multiply
+        case divide
+        func doCalculate(_ number1: Double, _ number2: Double) -> String {
+            switch  self {
+            case .plus:
+                return "\(number1 + number2)"
+            case .minus:
+                return "\(number1 - number2)"
+            case .multiply:
+                return "\(number1 * number2)"
+            case .divide:
+                guard number2 != 0 else {
+                    return "割る数には0以外を入力して下さい"
+                }
+                return "\(number1 / number2)"
+            }
+        }
     }
 
+    @IBAction private func calculateButton(_ sender: Any) {
+        let number1 = Double(num1TextField.text ?? "") ?? 0
+        let number2 = Double(num2TextField.text ?? "") ?? 0
 
+        if let operatorIndex = OperatorEnum(rawValue: operatorSegmentedControl.selectedSegmentIndex) {
+            let resultNum = operatorIndex.doCalculate(number1, number2)
+            resultLabel.text = resultNum
+        }
+    }
 }
-
